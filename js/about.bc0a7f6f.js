8ddea9fbc03b05809d2b147a94551426f6604397
Bamidele <bamsjovicy@gmail.com>
2022-12-09 01:47:47 +0100

    deploy

---
+++ js/about.bc0a7f6f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[443],{1272:function(t,n,u){u.r(n),u.d(n,{default:function(){return o}});var s=function(){var t=this;t._self._c;return t._m(0)},l=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],r=u(1001),e={},i=(0,r.Z)(e,s,l,!1,null,null,null),o=i.exports}}]);
+//# sourceMappingURL=about.bc0a7f6f.js.map
